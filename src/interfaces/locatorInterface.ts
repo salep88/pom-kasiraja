ecc7cdee4c48e0b39311d63d327d6ca39f18f689
Unang <unang@softwareseni.com>
2025-05-21 12:09:10 +0700

    fix typo

--- src/interfaces/locatorInterface.ts
+++ src/interfaces/locatorInterface.ts
@@ -1,9 +1,9 @@
 export interface LocatorRoleName {
     role: 'link' | 'button' | 'textbox' | 'combobox' | 'spinbutton' | 'gridcell' | 'menuitem' | 'alert';
     name?: string;
-}
+};
 
 export interface LocatorLabelName {
     label: string;
     options?: object;
-}
+};
